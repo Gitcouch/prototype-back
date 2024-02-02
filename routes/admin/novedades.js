13e839bf0ff78d465c11d5cac34984e0065a7f46
¨Pablo <imaezart@gmail.com>
2024-02-02 01:27:16 -0300

    Avances prototype backend

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const novedadesModel = require('../../models/novedadesModel');
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-/* traer las novedades */
-router.get("/", async function (req, res, next) {
-
+router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
-
   res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
@@ -15,12 +12,26 @@ router.get("/", async function (req, res, next) {
 });
 
 router.get('/agregar', (req, res, next) => {
-  res.render('admin/agregar', { // agregar hbs
+  res.render('admin/agregar', { 
     layout: 'admin/layout',
-  }) // cierra render
-}); // cierra Get
+  }); 
+});
 
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadById(id);
+  res.redirect('admin/novedades', { 
+  }); 
+});
 
+router.get('/modificar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', { 
+    layout: 'admin/layout',
+    novedad
+  }); 
+});
 
 router.post('/agregar', async (req, res, next) => {
   try {
@@ -48,5 +59,25 @@ router.post('/agregar', async (req, res, next) => {
   }
 });
 
+router.post('/modificar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+      await novedadesModel.modificarNovedadById(obj, req.body.id);
+      res.redirect('/admin/novedades')
+    } 
+ 
+   catch (error) {
+    console.log(error);
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad',
+    });
+  }
+});
 
 module.exports = router;

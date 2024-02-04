79f9740979b15a708f281732f45145d43e029ab3
¨Pablo <imaezart@gmail.com>
2024-02-04 19:38:12 -0300

    Prototype

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
+var createError = require('http-errors');
+var express = require('express');
 var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 require("dotenv").config();
-var session = require("express-session");
-var fileUpload = require("express-file-upload");
+var session = require('express-session');
+var fileUpload = require('express-file-upload');
+var cors = require('cors');
 
-var indexRouter = require("./routes/index");
-var usuariosRouter = require("./routes/usuarios");
-var loginRouter = require("./routes/admin/login");
-var adminRouter = require("./routes/admin/novedades");
+var indexRouter = require('./routes/index');
+var usuariosRouter = require('./routes/usuarios');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -45,17 +47,16 @@ const secured = async (req, res, next) => {
   }
 };
 
-app.use(
-  fileUpload({
+app.use(fileUpload({
     useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+    tempFileDir: '/tmp/'
+  }));
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuariosRouter);
 app.use("/admin/login", loginRouter);
 app.use("/admin/novedades", secured, adminRouter);
+app.use("/api", cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

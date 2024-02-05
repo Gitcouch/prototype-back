86eb280eb82818a2c9d205e62d57b1449c57f74c
¨Pablo <imaezart@gmail.com>
2024-02-05 02:32:12 -0300

    Avances Backend prototype

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,21 @@
-var express = require('express');
+var express = require('express')
 
-var router = express.Router();
-var cloudinary = require('cloudinary').v2;
+var router = express.Router()
+var cloudinary = require('cloudinary').v2
 
-var novedadesModel = require('../models/novedadesModel');
-const transport = require('../mailtrap.config');
+var novedadesModel = require('../models/novedadesModel')
+const transport = require('../mailtrap.config')
 
 router.get('/novedades', async (req, res) => {
-  let novedades = await novedadesModel.getNovedades();
+  let novedades = await novedadesModel.getNovedades()
 
   novedades = novedades.map((novedad) => {
     if (novedad.img_id) {
       const imagen = cloudinary.url(novedad.img_id, {
-        width: 960,
+        width: 200,
         height: 200,
         crop: 'fill'
-      });
+      })
       return {
         ...novedad,
         imagen
@@ -26,10 +26,10 @@ router.get('/novedades', async (req, res) => {
       ...novedad,
       image: null
     }
-  });
+  })
 
-  res.json(novedades);
-});
+  res.json(novedades)
+})
 
 router.post('/contacto', async (req, res) => {
   const mail = {
@@ -46,6 +46,6 @@ router.post('/contacto', async (req, res) => {
     error: false,
     message: 'Mensaje enviado'
   })
-});
+})
 
-module.exports = router;
+module.exports = router

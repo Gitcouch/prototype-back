16974244b5314f95cc586b48886828b45a918eb7
¨Pablo <imaezart@gmail.com>
2024-02-05 00:57:07 -0300

    api ok

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -1,45 +1,48 @@
+/* eslint-disable no-useless-catch */
 var pool = require('./bd'); // llamado a la base de datos
 
 async function getNovedades() {
-  var query = "select * from novedades order by id desc";
+  var query = 'select * from novedades order by id desc';
   var rows = await pool.query(query);
 
   return rows;
 }
 async function insertNovedad(obj) {
   try {
-    var query = "insert into novedades set ?";
+    var query = 'insert into novedades set ?';
     var rows = await pool.query(query, [obj])
 
     return rows;
-
-  } catch (error) {
+  }
+  catch (error) {
     console.error(error);
     throw error;
   }
 }
 
 async function deleteNovedadById(id) {
-  var query = "delete from novedades where id = ?";
+  var query = 'delete from novedades where id = ?';
   var rows = await pool.query(query, [id]);
   return rows;
 }
 
 async function getNovedadById(id) {
-  var query = "select * from novedades where id = ?";
+  var query = 'select * from novedades where id = ?';
   var rows = await pool.query(query, [id]);
   return rows[0];
 }
 
 async function modificarNovedadById(obj, id) {
   try {
-    var query = "update novedades set ? where id = ?";
+    var query = 'update novedades set ? where id = ?';
     var rows = await pool.query(query, [obj, id]);
     return rows;
-  } catch (error) {
+  }
+  catch (error) {
     throw error;
   }
 }
 
-
-module.exports = { getNovedades, deleteNovedadById, insertNovedad, getNovedadById, modificarNovedadById };
+module.exports = {
+  getNovedades, deleteNovedadById, insertNovedad, getNovedadById, modificarNovedadById
+};
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,44 +1,47 @@
-var express = require("express");
-var router = express.Router();
+var express = require('express')
 
-var usuariosModel = require("./../../models/usuariosModel");
+var router = express.Router()
+
+var usuariosModel = require('../../models/usuariosModel')
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("admin/login", {
+router.get('/', (req, res) => {
+  res.render('admin/login', {
     // Llama a login.hbs
-    layout: "admin/layout",
-  });
-});
+    layout: 'admin/layout'
+  })
+})
 
-router.get("/logout", function (req, res, next) {
-  req.session.destroy(); // destuye las variables de sección
-  res.render("admin/login", {
+router.get('/logout', (req, res) => {
+  req.session.destroy() // destuye las variables de sección
+  res.render('admin/login', {
     // Llama a login.hbs
-    layout: "admin/layout",
-  });
-});
+    layout: 'admin/layout'
+  })
+})
 
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const { user, password } = req.body; // Capturando Flavia,  Captura 1234
+    const { user, password } = req.body // Capturando Flavia,  Captura 1234
 
-    var data = await usuariosModel.getUserByUsernameAndPassword(user, password);
+    var data = await usuariosModel.getUserByUsernameAndPassword(user, password)
 
-    if (!!data) {
-      req.session.id_user = data.id; // id nombre de la columna de la tabla en base de datos
-      req.session.nombre = data.user; // user nombre de la columna de la tabla en base de datos
-      res.redirect("/admin/novedades");
-    } else {
-      res.render("admin/login", {
-        layout: "admin/layout",
-        error: true,
-      });
+    if (data) {
+      req.session.id_user = data.id // id nombre de la columna de la tabla en base de datos
+      req.session.nombre = data.user // user nombre de la columna de la tabla en base de datos
+      res.redirect('/admin/novedades')
     }
-  } catch (error) {
-    console.log(error);
-    next(error); // Pasa el error al siguiente middleware
+    else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      })
+    }
   }
-});
+  catch (error) {
+    console.log(error)
+    next(error) // Pasa el error al siguiente middleware
+  }
+})
 
-module.exports = router;
+module.exports = router
